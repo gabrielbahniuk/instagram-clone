5c743a207c8fbd785675922a51be60e3da93c32b
Gabriel <gabrieldiasbnu@hotmail.com>
2019-06-16 02:45:10 +0200

    First commit

---
+++ backend/src/config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+  dbUsername: '',
+  dbPassword: '',
+  dbServer: '',
+  dbInstance: ''
+};
---
+++ backend/src/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const {
+  dbUsername,
+  dbPassword,
+  dbServer,
+  dbInstance
+} = require('./config/database');
+const app = express();
+
+app.use((req, res, next) => {
+  req.io = io;
+  return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(
+  '/files',
+  express.static(path.resolve(__dirname, '..', 'uploads', 'resized'))
+);
+
+mongoose.connect(
+  `mongodb+srv://${dbUsername}:${dbPassword}@${dbServer}/${dbInstance}?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true
+  }
+);
+
+app.use(require('./routes'));
+
+server.listen(3333);
